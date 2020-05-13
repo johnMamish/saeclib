0d6632869d2781d9f3c52d6a7672c2619f6d8244
John Mamish <john.mamish@gmail.com>
2020-05-13 12:47:37 -0500

    add static array struct

---
+++ src/saeclib_static_array.h
@@ -0,0 +1,38 @@
+#ifndef _SAECLIB_STATIC_ARRAY_H
+#define _SAECLIB_STATIC_ARRAY_H
+
+/**
+ * Container for a resizable array with a fixed maximum capacity.
+ *
+ * Doesn't necessarily provide many useful functions, just a useful interface
+ */
+
+
+typedef struct saeclib_static_array
+{
+    // we use uint8_t as the base type for storing data as it's guaranteed to have sizeof() 1.
+    uint8_t* data;
+
+    // capacity is the total number of elements with size elt_size that can be held.
+    size_t capacity;
+
+    // current size of the array in number of elements.
+    size_t size;
+
+    // size in bytes of each element in the array
+    size_t elt_size;
+} saeclib_static_array_t;
+
+
+#define saeclib_static_array_salloc(capacity, eltsize) \
+    ({\
+    saeclib_static_array_t ssa; \
+    static uint8_t space[(capacity) * (elt_size)]; \
+    ssa.data = space; \
+    ssa.capacity = capacity; \
+    ssa.size = 0; \
+    ssa.elt_size = (eltsize); \
+    ssa; \
+    })
+
+#endif
