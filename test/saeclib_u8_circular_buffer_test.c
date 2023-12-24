a68e2fc1f519197907edb3c2c2fdc5a7e4ac50d0
John Mamish <john.mamish@gmail.com>
2023-12-23 23:01:00 -0500

    add fuzz test for popmany / pushmany functions

--- test/saeclib_u8_circular_buffer_test.c
+++ test/saeclib_u8_circular_buffer_test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #include "unity.h"
 
@@ -234,7 +235,7 @@ void saeclib_u8_circular_buffer_pushpopmany_wrap_test()
         TEST_ASSERT_EQUAL_INT(SAECLIB_ERROR_NOERROR, err);
         TEST_ASSERT_EQUAL_INT(0, saeclib_u8_circular_buffer_size(&scb));
     }
-    
+
     for (size_t j = 0; j < 7; j++) {
         TEST_ASSERT_EQUAL_INT(j, out[j]);
     }
@@ -291,15 +292,100 @@ void saeclib_u8_circular_buffer_popmany_underflow_test()
     TEST_ASSERT_EQUAL_INT(5, saeclib_u8_circular_buffer_size(&scb));
 
     uint8_t out[5] = { 0 };
-    
+
     err = saeclib_u8_circular_buffer_popmany(&scb, out, 5);
     TEST_ASSERT_EQUAL_INT(SAECLIB_ERROR_NOERROR, err);
     TEST_ASSERT_EQUAL_INT(0, saeclib_u8_circular_buffer_size(&scb));
-    
+
     err = saeclib_u8_circular_buffer_popmany(&scb, out, 5);
     TEST_ASSERT_EQUAL_INT(SAECLIB_ERROR_UNDERFLOW, err);
     TEST_ASSERT_EQUAL_INT(0, saeclib_u8_circular_buffer_size(&scb));
 
+#undef NUMEL
+}
+
+void saeclib_u8_circular_buffer_pushmany_popmany_fuzz_test0()
+{
+#define NUMEL 129
+    srand(0);
+
+    saeclib_u8_circular_buffer_t scb = saeclib_u8_circular_buffer_salloc(NUMEL);
+
+    uint32_t push_base = 0;
+    uint32_t incr_count = 0;
+    uint32_t golden_total = 0;
+    uint32_t running_total = 0;
+
+    for (int i = 0; i < 100000; i++) {
+        int action = rand() % 100;
+
+        if (action < 45) {
+            // action e [0, 45) means that we push a random number of elements that won't cause an
+            // overflow.
+            const int remaining_space =
+                (saeclib_u8_circular_buffer_capacity(&scb) - saeclib_u8_circular_buffer_size(&scb) - 1);
+            if (remaining_space == 0)
+                continue;
+            int push_num = rand() % remaining_space;
+
+            // we want to usually use small numbers; work in short chunks most of the time
+            if ((rand() % 100) < 90) {
+                push_num >>= 1;
+                if (push_num == 0) push_num = 1;
+            }
+
+            // construct the buffer from which to push
+            uint8_t pushbuf[NUMEL] = { 0 };
+            for (int j = 0; j < push_num; j++) pushbuf[j] = (push_base + j);
+            push_base += push_num;
+
+            // push
+            saeclib_error_e err = saeclib_u8_circular_buffer_pushmany(&scb, pushbuf, push_num);
+            TEST_ASSERT_EQUAL_INT(SAECLIB_ERROR_NOERROR, err);
+        } else if (action < 90) {
+            // action e [45, 90), we pop a random number of elements that won't cause an underflow.
+            // we also validate that we popped the correct data.
+            const int used_space = saeclib_u8_circular_buffer_size(&scb);
+            if (used_space == 0)
+                continue;
+            int pop_num = rand() % used_space;
+
+            // we want to usually use small numbers; work in short chunks most of the time
+            if ((rand() % 100) < 90) {
+                pop_num >>= 1;
+                if (pop_num == 0) pop_num = 1;
+            }
+
+            // pop the data
+            uint8_t popbuf[NUMEL] = { 0 };
+            saeclib_error_e err = saeclib_u8_circular_buffer_popmany(&scb, popbuf, pop_num);
+            TEST_ASSERT_EQUAL_INT(SAECLIB_ERROR_NOERROR, err);
+
+            // update golden example
+            for (int j = 0; j < pop_num; j++) golden_total += (uint8_t)(incr_count + j);
+            incr_count += pop_num;
+
+            // update our running total and compare with golden example.
+            for (int j = 0; j < pop_num; j++) running_total += popbuf[j];
+            TEST_ASSERT_EQUAL_INT(golden_total, running_total);
+        } else if (action < 95) {
+            // action e [90, 95) force a buffer overflow
+            const int remaining_space =
+                (saeclib_u8_circular_buffer_capacity(&scb) - saeclib_u8_circular_buffer_size(&scb) - 1);
+            uint8_t push_buf[NUMEL] = { 0 };
+            saeclib_error_e err = saeclib_u8_circular_buffer_pushmany(&scb, push_buf,
+                                                                      remaining_space + 1);
+            TEST_ASSERT_EQUAL_INT(SAECLIB_ERROR_OVERFLOW, err);
+        } else if (action < 100) {
+            // action e [95, 100) force a buffer underflow
+            const int used_space = saeclib_u8_circular_buffer_size(&scb);
+            uint8_t pop_buf[NUMEL] = { 0 };
+            saeclib_error_e err = saeclib_u8_circular_buffer_popmany(&scb, pop_buf,
+                                                                     used_space + 1);
+
+            TEST_ASSERT_EQUAL_INT(SAECLIB_ERROR_UNDERFLOW, err);
+        }
+    }
 #undef NUMEL
 }
 
@@ -318,5 +404,6 @@ int main(int argc, char** argv)
     RUN_TEST(saeclib_u8_circular_buffer_overflow_test);
     RUN_TEST(saeclib_u8_circular_buffer_pushmany_overflow_test);
     RUN_TEST(saeclib_u8_circular_buffer_popmany_underflow_test);
+    RUN_TEST(saeclib_u8_circular_buffer_pushmany_popmany_fuzz_test0);
     return UNITY_END();
 }
